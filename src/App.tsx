2edadf6c40736be198dfbb27f6f28d8f5962e8ae
sudhindrapai <sudhindrapai8@gmail.com>
2023-11-23 18:51:18 +0530

    modified css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-// import {TypeWritter} from 'typewritter';
-import TypeWritterEffect  from './components/TypeWritter/src/components/App';
+import {TypeWritter} from 'typewritter';
+// import TypeWritterEffect  from './components/TypeWritter/src/components/App';
 function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
-      {/* <TypeWritter text="Hi This is a new typewritting package in typescript!!" speed={100} fontSize={16} clearMessageSpeed={30} loop={true} /> */}
+      <TypeWritter text="Hi This is a new typewritting package in typescript!!" speed={100} fontSize={16} clearMessageSpeed={30} loop={true} />
       <h1>Local development</h1>
-      <TypeWritterEffect text="test" speed={100} fontSize={16} clearMessageSpeed={30} loop={true} />
+      {/* <TypeWritterEffect text="test" speed={100} fontSize={16} clearMessageSpeed={30} loop={true} /> */}
     </div>
   );
 }
--- src/components/TypeWritter/src/components/App.tsx
+++ src/components/TypeWritter/src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { FC, useEffect, useState } from 'react';
-import classes from 'App.module.css';
+import { FC, useEffect, useState } from 'react'
+// import classes from 'App.module.css'
 
 type TypeWrittingProps = {
   text: string
@@ -51,7 +51,7 @@ const TypeWrittingEffect: FC<TypeWrittingProps> = (props): any => {
     <>
       <div style={{ fontSize: fontSize }}>
         {message}
-        <span className={classes.Cursor} style={{ height: fontSize}}></span>
+        <span style={{ height: fontSize }}></span>
       </div>
     </>
   )
